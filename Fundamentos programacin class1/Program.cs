cfa5bf192fd53f784b2d54490e0ef316cfd6e678
vanessa4512 <vanesita1208@gmail.com>
2025-08-22 13:32:22 -0600

    22 08

--- Fundamentos programacin class1/Program.cs
+++ Fundamentos programacin class1/Program.cs
@@ -1,3 +1,68 @@
-﻿ // See https://aka.ms/new-console-template for more information
+﻿void banderas () {
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.Write("RU");
+    Console.BackgroundColor = ConsoleColor.DarkBlue;
+    Console.Write("S");
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.Write("IA");
 
-Console.WriteLine("Hello, World!");
+    Console.WriteLine();
+
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.Write("P");
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.Write("ER");
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.Write("U");
+    Console.ResetColor();
+}
+string separador = "----------------------";
+
+void intento1(){
+    string nombre = "<Nombre del jugador>";
+    int level = 15;
+    float exp = 13;
+
+    Console.WriteLine($"nombre: {nombre}" );
+    Console.WriteLine($"level: {level}");
+    Console.WriteLine($"exp: {exp}");
+    Console.WriteLine(separador);
+}
+
+
+
+
+void intento2(){
+    string nombrenew = "Nombre del jugador";
+    int levelNew = 17;    
+    long expNew = 8_439_581_839;
+    sbyte pociones = 6;
+    string arma = "espada sagrada";
+    Console.WriteLine($"nombre: {nombrenew}");
+    Console.WriteLine($"level: {levelNew}");
+    Console.WriteLine($"exp: {expNew}");
+    Console.WriteLine($"pociones: {pociones}");
+    Console.WriteLine($"arma: {arma}");
+    Console.WriteLine(separador);
+}
+
+void imprimiratrbutos1(){
+    string nombrenew = "Nombre del jugador";
+    int levelNew = 17;    
+    long expNew = 8_439_581_839;
+    sbyte pociones = 6;
+    string arma = "espada sagrada";
+    int hp = 100;
+    int mp = 50;
+    int oro = 4712;
+
+    Console.WriteLine("STARFIRE");
+    Console.WriteLine(separador);
+    Console.WriteLine($"nombre:\t{nombrenew}");
+    Console.WriteLine($"HP:\t{hp}");
+    Console.WriteLine($"MP:\t{mp}");
+    Console.WriteLine($"oro:\t{oro}");
+    Console.WriteLine($"exp:\t{expNew}");
+    Console.WriteLine(separador);
+}
